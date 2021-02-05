765848651d8de9540d6b7a01bae3f00c2568eeaa
Arthur Noellec <mac-pro@MBP-de-Arthur.lan>
2021-02-05 20:54:15 +0100

    ajout du pregramme FizzBuzz

---
+++ src/FizzBuzz.java
@@ -0,0 +1,20 @@
+public class FizzBuzz {
+
+
+    public static void main(String[] args) {
+
+
+
+        for ( int nbr=1 ; nbr < 100 ; nbr++){
+            if (nbr%3 == 0 && nbr%5 !=0) {
+                System.out.println(nbr + " : Fizz");
+            }else if (nbr%5 == 0 && nbr%3 !=0){
+                System.out.println(nbr + " : Buzz");
+            }else if ( nbr%3==0 && nbr%5==0){
+                System.out.println(nbr + " : FizzBuzz");
+            }
+        }
+
+
+    }
+}
